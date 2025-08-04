9bed66c9589e3a0cba644b6343914f47352dbdd2
hguarido55 <hguarido@gmail.com>
2025-08-04 17:51:30 +0200

    Avances en el tutorial - mostly frontend

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -15,7 +15,24 @@ export const useProductStore = create((set) => ({
             body:JSON.stringify(newProduct)
         })
         const data = await res.json();
-        set((state) => ({products:[...state.products,data.data]}))
-        return {success: true, message: "Product created successfully"}
-    }
+        set((state) => ({products:[...state.products,data.data]}));
+        return {success: true, message: "Product created successfully"};
+    },
+
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data });
+    },
+
+    deleteProduct: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        if(!data.success) return {success: false, message: data.message};
+
+        set(state => ({ products: state.products.filter(product => product._id !== pid) }));
+        return {success: true, message: data.message};
+    },
 }));
